f5cc50a890587ef931500e5a9229d3ff67c8167e
farisqlail <faris.riskilail@gmail.com>
2022-02-16 12:14:06 +0700

    users table

---
+++ app/Database/Migrations/2022-02-16-050749_Users.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100
+            ],
+            'phone_no' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 120
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
